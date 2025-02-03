58bcc7cc5f32b6ab530fe1ce26be1a8834210bfc
AndisPerime <andisbudzilko@gmail.com>
2025-02-03 14:14:36 +0000

    Add game.js and update index.html with game buttons and styles

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+// Game choices and rules object
+const gameRules = {
+    rock: { beats: ['scissors', 'lizard'] },
+    paper: { beats: ['rock', 'spock'] },
+    scissors: { beats: ['paper', 'lizard'] },
+    lizard: { beats: ['paper', 'spock'] },
+    spock: { beats: ['scissors', 'rock'] }
+};
