f546736c01052392f166c16befe17e0aef8efe74
AndisPerime <andisbudzilko@gmail.com>
2025-02-03 14:15:24 +0000

    Add image paths for game choices in game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,3 +6,12 @@ const gameRules = {
     lizard: { beats: ['paper', 'spock'] },
     spock: { beats: ['scissors', 'rock'] }
 };
+
+// Image paths for game choices
+const images = {
+    rock: 'assets/images/rock.png',
+    paper: 'assets/images/paper.png',
+    scissors: 'assets/images/scissors.png',
+    lizard: 'assets/images/lizard.png',
+    spock: 'assets/images/spock.png'
+};
